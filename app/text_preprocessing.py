1fa5ce3aff23e2bdda857ea3a1b369d66c319c58
Haseeb <haseeb.shaikh@helsinki.fi>
2025-06-14 17:52:08 +0300

    Fix NLTK resource loading for Streamlit Cloud

--- app/text_preprocessing.py
+++ app/text_preprocessing.py
@@ -1,19 +1,25 @@
 import nltk
-
-nltk.download('punkt')
-nltk.download('stopwords')
-
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
 
 stop_words = set(stopwords.words('english'))
 
 def preprocess_text(text):
     text = re.sub(r"[^a-zA-Z]", " ", text)
-    
+
     words = word_tokenize(text.lower())
-    
+
     filtered_words = [w for w in words if w not in stop_words and len(w) > 1]
-    
+
     return " ".join(filtered_words)
